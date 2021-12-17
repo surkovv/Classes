ffaf150b6f4159be3ec492f674c02bf763947bf9
surkovv <surkov.vo@phystech.edu>
2021-12-17 21:43:50 +0300

    media-fix

--- classes/src/components/CourseCard.js
+++ classes/src/components/CourseCard.js
@@ -6,7 +6,7 @@ import {backend} from "../config";
 function CourseCard({info}) {
     return <Link to={`/courses/${info.id}`}>
     <div className='course_card'>
-        <img className='course_symbol' src={backend + info.img_url} alt={info.name}/>
+        <img className='course_symbol' src={backend.slice(0, -1) + info.img_url} alt={info.name}/>
         <div className='name_description_holder'>
             <p className='course_card_name'>{info.name}</p>
             <p className='course_card_description'>{info.description}</p>
